3e015620b4a80a0122351ea15f87bd60407b90ff
kobecheong <kobecheong@MY-IT-011.office.local>
2019-05-27 10:08:27 +0800

    API Call

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import {
+  Text,
+  View,
+  Button
+} from "react-native";
 
 export default class SettingsScreen extends React.Component {
+  state = {res: ''};
   static navigationOptions = {
     title: 'app.json',
   };
 
+  getWeather = () => {
+    // const responds = await fetch ('https://www.metaweather.com/api/location/search//query=london');
+    // const jsonDate = await responds.json();
+    // this.setState({weather:jsonDate});
+
+    fetch('https://www.metaweather.com/api/location/search/?query=london')
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      this.setState({weather: json});
+    });
+  };
+
   render() {
+    const res = 'ABC';
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+        <View>
+          <Text>{this.state.json}</Text>
+          <Button
+                title="Check"
+                onPress={this.getWeather}
+              />
+        </View>
+      );
   }
 }
